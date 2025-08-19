26a8475b59b9683940ef212c9227b31a72bd7e09
Drex <nicknamedrex@gmail.com>
2025-08-19 02:08:25 +0200

    Rewrite to improve performance

--- src/main/java/me/drex/quickpack/mixin/FileResourcesSupplierMixin.java
+++ src/main/java/me/drex/quickpack/mixin/FileResourcesSupplierMixin.java
@@ -1,7 +1,6 @@
 package me.drex.quickpack.mixin;
 
 import me.drex.quickpack.packs.FastFilePackResources;
-import net.minecraft.server.packs.CompositePackResources;
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackLocationInfo;
 import net.minecraft.server.packs.PackResources;
@@ -12,43 +11,27 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.zip.ZipFile;
 
 @Mixin(FilePackResources.FileResourcesSupplier.class)
 public abstract class FileResourcesSupplierMixin {
     @Shadow @Final private File content;
 
-//    @Redirect(
-//        method = "openFull",
-//        at = @At(
-//            value = "NEW",
-//            target = "(Lnet/minecraft/server/packs/PackLocationInfo;L;Ljava/lang/String;)Lnet/minecraft/server/packs/FilePackResources;"
-//        )
-//    )
-//    public PackResources useFastFilePackResources(PackLocationInfo packLocationInfo, FilePackResources.SharedZipFileAccess sharedZipFileAccess, String string) {
-//        return new FastFilePackResources(packLocationInfo, sharedZipFileAccess, string);
-//    }
-
     /**
      * @author drex
-     * @reason Use FastFilePackResources, for some reason redirect doesn't work :/
+     * @reason Use optimized FastFilePackResources
      */
     @Overwrite
     public PackResources openFull(PackLocationInfo packLocationInfo, Pack.Metadata metadata) {
-        FilePackResources.SharedZipFileAccess sharedZipFileAccess = new FilePackResources.SharedZipFileAccess(this.content);
-        PackResources primaryPackResources = new FastFilePackResources(packLocationInfo, sharedZipFileAccess, "");
-        List<String> overlays = metadata.overlays();
-        if (overlays.isEmpty()) {
-            return primaryPackResources;
-        } else {
-            List<PackResources> packResourcesStack = new ArrayList<>(overlays.size());
-
-            for (String overlay : overlays) {
-                packResourcesStack.add(new FastFilePackResources(packLocationInfo, sharedZipFileAccess, overlay));
-            }
-
-            return new CompositePackResources(primaryPackResources, packResourcesStack);
+        ZipFile zipFile = null;
+        try {
+            zipFile = new ZipFile(this.content);
+        } catch (IOException e) {
+            FastFilePackResources.LOGGER.error("Failed to open pack {}", this.content, e);
         }
+        List<String> overlays = metadata.overlays();
+        return new FastFilePackResources(packLocationInfo, zipFile, overlays);
     }
 }
--- src/main/java/me/drex/quickpack/packs/FastFilePackResources.java
+++ src/main/java/me/drex/quickpack/packs/FastFilePackResources.java
@@ -1,13 +1,10 @@
 package me.drex.quickpack.packs;
 
 import com.mojang.logging.LogUtils;
-import me.drex.quickpack.mixin.SharedZipFileAccessAccessor;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.AbstractPackResources;
-import net.minecraft.server.packs.FilePackResources;
-import net.minecraft.server.packs.PackLocationInfo;
-import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.*;
 import net.minecraft.server.packs.resources.IoSupplier;
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
@@ -19,23 +16,29 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class FastFilePackResources extends AbstractPackResources {
-    static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     private final TreeMap<String, byte[]> fileMap = new TreeMap<>();
-
     private final Map<String, Set<String>> namespaces = new HashMap<>();
-    private final FilePackResources.SharedZipFileAccess sharedZipFileAccess;
-    private final String prefix;
+    private final ZipFile zipFile;
+    private final List<String> prefixStack;
+    private final Set<String> overlays;
 
-    public FastFilePackResources(PackLocationInfo packLocationInfo, FilePackResources.SharedZipFileAccess sharedZipFileAccess, String prefix) {
+    public FastFilePackResources(PackLocationInfo packLocationInfo, ZipFile zipFile, List<String> overlays) {
         super(packLocationInfo);
-        this.sharedZipFileAccess = sharedZipFileAccess;
-        this.prefix = prefix;
-        extractFiles(sharedZipFileAccess);
+        this.zipFile = zipFile;
+
+        this.overlays = new HashSet<>(overlays);
+        prefixStack = new ArrayList<>(overlays.size() + 1);
+        for (int i = overlays.size() - 1; i >= 0; i--) {
+            prefixStack.add(overlays.get(i) + "/");
+        }
+        prefixStack.add("");
+
+        extractFiles();
     }
 
-    public void extractFiles(FilePackResources.SharedZipFileAccess sharedZipFile) {
-        ZipFile zipFile = ((SharedZipFileAccessAccessor) sharedZipFile).invokeGetOrCreateZipFile();
+    public void extractFiles() {
         if (zipFile == null) return;
         Enumeration<? extends ZipEntry> entries = zipFile.entries();
         while (entries.hasMoreElements()) {
@@ -43,28 +46,7 @@ public class FastFilePackResources extends AbstractPackResources {
             if (entry.isDirectory()) continue;
 
             String path = entry.getName();
-            String[] parts = path.split("/");
-
-            if (!prefix.isEmpty()) {
-                if (parts.length == 0) continue;
-                if (!parts[0].equals(prefix)) continue;
-            }
-
-            int startIndex;
-            if (prefix.isEmpty()) {
-                startIndex = 0;
-            } else {
-                startIndex = 1;
-            }
-            if (parts.length > startIndex + 2) {
-                String type = parts[startIndex];
-                String namespace = parts[startIndex + 1];
-                if (ResourceLocation.isValidNamespace(namespace)) {
-                    namespaces.computeIfAbsent(type, s -> new HashSet<>()).add(namespace);
-                } else {
-                    LOGGER.warn("Non [a-z0-9_.-] character in namespace {} in pack {}, ignoring", namespace, ((SharedZipFileAccessAccessor) sharedZipFile).getFile());
-                }
-            }
+            extractNamespace(path);
 
             try (InputStream in = zipFile.getInputStream(entry)) {
                 byte[] data = in.readAllBytes();
@@ -75,41 +57,66 @@ public class FastFilePackResources extends AbstractPackResources {
         }
     }
 
+    private void extractNamespace(String path) {
+        String[] parts = path.split("/");
+        if (parts.length == 0) return;
+
+        boolean isOverlay = overlays.contains(parts[0]);
+        String type;
+        String namespace;
+        if (isOverlay && parts.length >= 3) {
+            type = parts[1];
+            namespace = parts[2];
+        } else if (!isOverlay && parts.length >= 2) {
+            type = parts[0];
+            namespace = parts[1];
+        } else {
+            return;
+        }
+
+        if (ResourceLocation.isValidNamespace(namespace)) {
+            namespaces.computeIfAbsent(type, s -> new HashSet<>()).add(namespace);
+        } else {
+            LOGGER.warn("Non [a-z0-9_.-] character in namespace {} in pack {}, ignoring", namespace, zipFile);
+        }
+    }
+
     @Override
     public @Nullable IoSupplier<InputStream> getRootResource(String... parts) {
-        return getResource(String.join("/", parts));
-    }
-
-    @Override
-    public @Nullable IoSupplier<InputStream> getResource(PackType packType, ResourceLocation resourceLocation) {
-        return getResource(packType.getDirectory() + "/" + resourceLocation.getNamespace() + "/" + resourceLocation.getPath());
-    }
-
-    private String addPrefix(String string) {
-        return this.prefix.isEmpty() ? string : this.prefix + "/" + string;
-    }
-
-    @Nullable
-    private IoSupplier<InputStream> getResource(String filePath) {
-        byte[] data = fileMap.get(addPrefix(filePath));
+        byte[] data = fileMap.get(String.join("/", parts));
         if (data == null) return null;
         return () -> new ByteArrayInputStream(data);
     }
 
     @Override
+    public @Nullable IoSupplier<InputStream> getResource(PackType packType, ResourceLocation resourceLocation) {
+        for (String prefix : prefixStack) {
+            byte[] data = fileMap.get(prefix + packType.getDirectory() + "/" + resourceLocation.getNamespace() + "/" + resourceLocation.getPath());
+            if (data == null) continue;
+            return () -> new ByteArrayInputStream(data);
+        }
+        return null;
+    }
+
+    @Override
     public void listResources(PackType packType, String namespace, String path, ResourceOutput resourceOutput) {
-        String namespacePrefix = addPrefix(packType.getDirectory() + "/" + namespace + "/");
-        String dirPrefix = namespacePrefix + path + "/";
-        String end = dirPrefix + Character.MAX_VALUE;
-        fileMap.subMap(dirPrefix, end).forEach((filePath, bytes) -> {
-            String rlPath = filePath.substring(namespacePrefix.length());
-            ResourceLocation location = ResourceLocation.tryBuild(namespace, rlPath);
-            if (location != null) {
-                resourceOutput.accept(location, () -> new ByteArrayInputStream(bytes));
-            } else {
-                LOGGER.warn("Invalid path in datapack: {}:{}, ignoring", namespace, rlPath);
-            }
-        });
+        Map<ResourceLocation, IoSupplier<InputStream>> map = new HashMap<>();
+
+        for (String prefix : prefixStack) {
+            String namespacePrefix = prefix + packType.getDirectory() + "/" + namespace + "/";
+            String dirPrefix = namespacePrefix + path + "/";
+            String end = dirPrefix + Character.MAX_VALUE;
+            fileMap.subMap(dirPrefix, end).forEach((filePath, bytes) -> {
+                String rlPath = filePath.substring(namespacePrefix.length());
+                ResourceLocation location = ResourceLocation.tryBuild(namespace, rlPath);
+                if (location != null) {
+                    map.putIfAbsent(location, () -> new ByteArrayInputStream(bytes));
+                } else {
+                    LOGGER.warn("Invalid path in datapack: {}:{}, ignoring", namespace, rlPath);
+                }
+            });
+        }
+        map.forEach(resourceOutput);
     }
 
     @Override
@@ -119,6 +126,8 @@ public class FastFilePackResources extends AbstractPackResources {
 
     @Override
     public void close() {
-        sharedZipFileAccess.close();
+        if (zipFile != null) {
+            IOUtils.closeQuietly(this.zipFile);
+        }
     }
 }
