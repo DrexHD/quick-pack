5abd0d562201b4110083966d6163845cfa21d987
Drex <nicknamedrex@gmail.com>
2026-07-07 01:59:08 +0200

    Update to forge 26.1.2

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -1,19 +1,12 @@
-buildscript {
-    repositories {
-        maven("https://maven.minecraftforge.net")
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath(group= "net.minecraftforge.gradle", name="ForgeGradle", version = "5.1.+")
-        classpath("org.spongepowered:mixingradle:0.7.+")
-    }
+plugins {
+    id("net.minecraftforge.gradle") version "7.0.31"
+    id("multiloader-loader")
 }
 
-plugins {
-    id("net.minecraftforge.gradle") version "6.0.36"
-    id("org.spongepowered.mixin") version "0.7.+"
-    id("multiloader-loader")
+repositories {
+    minecraft.mavenizer(this)
+    maven(fg.forgeMaven)
+    maven(fg.minecraftLibsMaven)
 }
 
 version = "forge-${project.property("mod_version")}+${project.property("minecraft_version")}"
@@ -22,30 +15,22 @@ base {
     archivesName = "${project.property("archives_base_name")}"
 }
 
-minecraft {
-    mappingChannel = "official"
-    mappingVersion = "${project.property("minecraft_version")}"
-
-    copyIdeResources = true
-}
-
 dependencies {
-    minecraft("net.minecraftforge:forge:${project.property("minecraft_version")}-${project.property("forge_version")}")
+    implementation(minecraft.dependency("net.minecraftforge:forge:${project.property("minecraft_version")}-${project.property("forge_version")}"))
 
-    compileOnly(annotationProcessor("org.spongepowered:mixin:0.8.7:processor")!!)
-    compileOnly(annotationProcessor("io.github.llamalad7:mixinextras-common:0.5.0")!!)
-    implementation(jarJar("io.github.llamalad7:mixinextras-forge:0.5.0")!!).apply {
-        jarJar.ranged(this, "[0.5.0,)")
-    }
+    compileOnly(annotationProcessor("io.github.llamalad7:mixinextras-common:0.5.3")!!)
+    implementation("io.github.llamalad7:mixinextras-forge:0.5.3")
 }
 
-mixin {
-    add(sourceSets.main.get(), "quick-pack.refmap.json")
-    config("quick-pack.mixins.json")
+
+tasks.jar {
+    manifest.attributes(mapOf(
+        "MixinConfigs" to "quick-pack.mixins.json"
+    ))
 }
 
 publishMods {
-    file.set(tasks.jarJar.get().archiveFile)
+    file.set(tasks.jar.get().archiveFile)
 
     displayName.set("quick-pack ${version.get()}")
     modLoaders.addAll("forge")
