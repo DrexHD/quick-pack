1750636ae4b9c3f33c057babdd19918ed3157bcc
Julian Chang <wyvest@polyfrost.org>
2026-08-08 05:28:27 +0700

    improve mixin

--- common/src/main/java/me/drex/quickpack/mixin/SpriteLoaderMixin.java
+++ common/src/main/java/me/drex/quickpack/mixin/SpriteLoaderMixin.java
@@ -1,24 +1,20 @@
 package me.drex.quickpack.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.drex.quickpack.config.ConfigManager;
 import net.minecraft.client.renderer.texture.SpriteLoader;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 @Mixin(SpriteLoader.class)
 public abstract class SpriteLoaderMixin {
-
-    @Unique
-    private Executor quick_pack$mipmapExecutor = null;
 
     @WrapOperation(
         method = "stitch",
@@ -27,37 +23,20 @@ public abstract class SpriteLoaderMixin {
             target = "Ljava/util/concurrent/CompletableFuture;runAsync(Ljava/lang/Runnable;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"
         )
     )
-    private CompletableFuture<Void> skipSerialMipmapGeneration(Runnable task, Executor executor, Operation<CompletableFuture<Void>> original) {
+    private CompletableFuture<Void> parallelMipmapGeneration(
+        Runnable task,
+        Executor executor,
+        Operation<CompletableFuture<Void>> original,
+        @Local Map<?, TextureAtlasSprite> regions,
+        @Local(ordinal = 6) int mipLevel
+    ) {
         if (!ConfigManager.config.parallelMipmapGeneration) {
             return original.call(task, executor);
         }
-        this.quick_pack$mipmapExecutor = executor;
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @ModifyReturnValue(method = "stitch", at = @At("RETURN"))
-    private SpriteLoader.Preparations parallelMipmapGeneration(SpriteLoader.Preparations preparations) {
-        Executor executor = this.quick_pack$mipmapExecutor;
-        if (executor == null) {
-            return preparations;
-        }
-        this.quick_pack$mipmapExecutor = null;
-
-        Collection<TextureAtlasSprite> sprites = preparations.regions().values();
-        int mipLevel = preparations.mipLevel();
-        CompletableFuture<Void> readyForUpload = CompletableFuture.allOf(
-            sprites.stream()
+        return CompletableFuture.allOf(
+            regions.values().stream()
                 .map(sprite -> CompletableFuture.runAsync(() -> sprite.contents().increaseMipLevel(mipLevel), executor))
                 .toArray(CompletableFuture[]::new)
-        );
-
-        return new SpriteLoader.Preparations(
-            preparations.width(),
-            preparations.height(),
-            mipLevel,
-            preparations.missing(),
-            preparations.regions(),
-            readyForUpload
         );
     }
 }
