ec1daeaae4863c9343e6fd30dc3c81016ecb031a
Drex <nicknamedrex@gmail.com>
2026-08-10 15:29:48 +0200

    Parallelize mipmap generation

---
+++ common/src/main/java/me/drex/quickpack/mixin/SpriteLoaderMixin.java
@@ -0,0 +1,40 @@
+package me.drex.quickpack.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.drex.quickpack.config.ConfigManager;
+import net.minecraft.client.renderer.texture.SpriteLoader;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+
+@Mixin(SpriteLoader.class)
+public abstract class SpriteLoaderMixin {
+    @WrapOperation(
+        //? if >= 26.1 {
+        method = "lambda$stitch$2",
+        //? } else {
+        /*method = {
+            "method_45840", // fabric
+            "lambda$stitch$3" // (neo)forge
+        },
+        *///? }
+        require = 1,
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/util/Collection;forEach(Ljava/util/function/Consumer;)V"
+        )
+    )
+    private static void parallelMipmapGeneration(
+        Collection<TextureAtlasSprite> instance, Consumer<? super TextureAtlasSprite> consumer, Operation<Void> original
+    ) {
+        if (!ConfigManager.config.parallelMipmapGeneration) {
+            original.call(instance, consumer);
+        } else {
+            instance.parallelStream().forEach(consumer);
+        }
+    }
+}
